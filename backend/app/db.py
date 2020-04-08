af98dfb718ead54a8686836d9ab4a351b6493e40
Andrew Joseph <andrew@aj-media.net>
2020-04-08 12:52:15 -0600

    db queries working

--- backend/app/db.py
+++ backend/app/db.py
@@ -19,34 +19,27 @@ def get_db():
     except:
         return "connection error"
 
-# # posts device token to db
-# def update_ski_area(name):
-#     try:
-#         db = get_db()
-#         cursor = db.cursor()
-#         cursor.execute("use snow_db")
-#         query = """ INSERT INTO ski_area VALUES ("%s"); """ % name
-#         cursor.execute(query)
-#         db.commit()
-#         return True
-#     except:
-#         return False
 
-
-# returns a ski areas data
+# returns all data
 def get_all_data():
     db = get_db()
     cursor = db.cursor()
     cursor.execute("use snow_db")
     cursor.execute(""" SELECT * FROM ski_area; """)
-    results = cursor.fetchall()
+    results = list(cursor.fetchall()) # convert outer tuple to list
+    results = [list(i) for i in results] # convert nested tuples to lists
+
+    for item in results: # pop id off each list
+        item.pop(0)
     return results
 
 
+# returns a ski areas data
 def get_ski_area(name):
     db = get_db()
     cursor = db.cursor()
     cursor.execute("use snow_db")
     cursor.execute(""" SELECT * FROM ski_area WHERE name="%s";  """ % name)
-    results = cursor.fetchall()
+    results = list(cursor.fetchall()[0])
+    results.pop(0)
     return results
