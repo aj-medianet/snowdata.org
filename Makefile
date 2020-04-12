0d9d656c5523fcabc6495f9d848b0a07ec7eabcc
Andrew Joseph <andrew@aj-media.net>
2020-04-12 12:38:10 -0600

    frontend can make request to backend and get json res

---
+++ Makefile
@@ -0,0 +1,12 @@
+build:
+	docker-compose build
+
+run:
+	docker-compose up
+
+rund:
+	docker-compose up -d
+
+br:
+	docker-compose up -d --build
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  getTest = (event) => {
+    console.log("GET test")
+    event.preventDefault()
+    fetch("http://localhost:7082")
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        console.log(data)
+      })
+  }
+
+  postTest = (event) => {
+    console.log("POST test")
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+      <div className="container-fluid p-5">
+        <div className="row">
+          <div className="col text-center">
+            <h1>API Test</h1>
+            <button className="btn btn-primary mt-3 mr-2" onClick={this.getTest}>GET Request Test</button>
+            <button className="btn btn-primary mt-3 ml-2" onClick={this.postTest}>POST Request Test</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
