6286809acb8ff3c114fa9cf652966b897c48a168
Andrew Joseph <andrew@aj-media.net>
2020-04-12 12:57:40 -0600

    post request from frontend to backend works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,20 +11,43 @@ class App extends Component {
   }
 
   getTest = (event) => {
-    console.log("GET test")
     event.preventDefault()
     fetch("http://localhost:7082")
-      .then((response) => {
-        return response.json()
-      })
-      .then((data) => {
-        console.log(data)
-      })
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      console.log(data)
+    })
   }
 
-  postTest = (event) => {
-    console.log("POST test")
+  getAllData = (event) => {
     event.preventDefault()
+    fetch('http://localhost:7082/get-all-data')
+    .then((response) => {
+      return response.json()
+    }).then((data) => {
+      console.log(data)
+    })
+  }
+
+  getSkiAreaData = (event) => {
+    event.preventDefault()
+    const data = { skiareaname : "Snowbird" }
+    //console.log(data)
+    //console.log(JSON.stringify(data))
+    fetch('http://localhost:7082/get-ski-area', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json',},
+      body: JSON.stringify(data),
+    }).then((response) => {
+      return response.json()
+    }).then((data) => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
   }
 
   render() {
@@ -34,7 +57,8 @@ class App extends Component {
           <div className="col text-center">
             <h1>API Test</h1>
             <button className="btn btn-primary mt-3 mr-2" onClick={this.getTest}>GET Request Test</button>
-            <button className="btn btn-primary mt-3 ml-2" onClick={this.postTest}>POST Request Test</button>
+            <button className="btn btn-primary mt-3 ml-2 mr-2" onClick={this.getAllData}>Get All Data</button>
+            <button className="btn btn-primary mt-3 ml-2" onClick={this.getSkiAreaData}>Get Snowbird Data</button>
           </div>
         </div>
       </div>
