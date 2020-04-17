6cffd74b67f818bc81f3d3630254bdc04e98fdf8
Andrew Joseph <andrew@aj-media.net>
2020-04-17 11:48:03 -0600

    added icon and cleaned up boostrap

--- frontend/src/components/HeaderContent.js
+++ frontend/src/components/HeaderContent.js
@@ -5,8 +5,8 @@ class HeaderContent extends Component {
         return (
             <header className="">
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <a class="navbar-brand" href="/">SnowData</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    <a className="navbar-brand" href="/">SnowData</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
