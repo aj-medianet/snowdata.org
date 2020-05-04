5b1280f11719214675242dcfb13cebc17ad3f7a9
Andrew Joseph <andrew@aj-media.net>
2020-05-04 08:16:19 -0600

    added ski area data to home page

--- frontend/src/components/MainContent.js
+++ frontend/src/components/MainContent.js
@@ -5,25 +5,57 @@ import React, { Component } from 'react';
 const getAllData = (event) => {
   event.preventDefault()
   fetch('https://api.snowdata.org/get-all-data/tmpkey')
-  //fetch('http://localhost:7082/get-all-data')
-  .then((response) => {
-    return response.json()
-  }).then((data) => {
-    console.log(data)  
-  })
+    //fetch('http://localhost:7082/get-all-data')
+    .then((response) => {
+      return response.json()
+    }).then((data) => {
+      console.log(data)
+    })
 }
 
 class MainContent extends Component {
   constructor() {
     super()
     this.state = {
+      skiareas: []
     }
   }
+
+  componentDidMount() {
+    const url = 'https://api.snowdata.org/get-all-data/tmpkey';
+    //const url = 'http://localhost:7082/get-all-data/tmpkey';
+    fetch(url)
+      .then(response => response.json())
+      .then(skiareas => this.setState({ skiareas }));
+  }
+
   render() {
     return (
-      
-      <h1>Snowdata.org Main Page</h1>
-      
+
+      <div>
+        <ul>
+          {
+            this.state.skiareas.map(area =>
+              <div key={area.id}>
+                {
+                  <div>
+                    <h1>{area.name}</h1>
+                    <div>Current temperature: {area.cur_temp}</div>
+                    <div>Wind speed: {area.wind_speed}</div>
+                    <div>Wind direction: {area.wind_dir}</div>
+                    <div>12 Hour snowfall: {area.new_snow_12}</div>
+                    <div>24 Hour snowfall: {area.new_snow_24}</div>
+                    <div>48 Hour snowfall: {area.new_snow_48}</div>
+                    <div>Current snow depth: {area.cur_depth}</div>
+                    <div>YTD: {area.ytd}</div>
+                  </div>
+                }
+              </div>
+            )
+          }
+        </ul>
+      </div>
+
     )
   }
 }
--- frontend/src/components/SkiAreas.js
+++ frontend/src/components/SkiAreas.js
@@ -1,32 +1,34 @@
 import React, { Component } from 'react';
 //import { useState, useEffect } from 'react';
 
-// displays individual skiareas
-class SkiAreas extends React.Component {
-    constructor(props) {
-        super(props);
-          this.state = {
-              skiareas: []
-        }
-      }
-    
-      componentDidMount() {  
-        const url = 'https://api.snowdata.org/get-all-data/tmpkey';
-        //const url = 'http://localhost:7082/get-all-data/tmpkey';
-      fetch(url)
+
+// TODO
+// dynamically loads a ski area depending on user choice from nav dropdown
+class SkiAreas extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      skiareas: []
+    }
+  }
+
+  componentDidMount() {
+    const url = 'https://api.snowdata.org/get-all-data/tmpkey';
+    //const url = 'http://localhost:7082/get-all-data/tmpkey';
+    fetch(url)
       .then(response => response.json())
-      .then(skiareas => this.setState({skiareas}));
-      }
-      
-    render() {
-      return (
-        <div>
-          <ul>
-            {
-              this.state.skiareas.map(area => 
-                <div key={area.id}>
-                  {
-                    <div>
+      .then(skiareas => this.setState({ skiareas }));
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {
+            this.state.skiareas.map(area =>
+              <div key={area.id}>
+                {
+                  <div>
                     <h1>{area.name}</h1>
                     <div>Current temperature: {area.cur_temp}</div>
                     <div>Wind speed: {area.wind_speed}</div>
@@ -36,17 +38,16 @@ class SkiAreas extends React.Component {
                     <div>48 Hour snowfall: {area.new_snow_48}</div>
                     <div>Current snow depth: {area.cur_depth}</div>
                     <div>YTD: {area.ytd}</div>
-                    </div>
-                  }
                   </div>
-              )
-            }
-          </ul>
-        </div>
-      );
-    }
-    }
+                }
+              </div>
+            )
+          }
+        </ul>
+      </div>
+    );
+  }
+}
 
 
 export default SkiAreas;
-    
