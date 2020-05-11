2235fb1333c9d0693302584b89ffa1a2a93318ba
Andrew Joseph <andrew@aj-media.net>
2020-05-10 21:31:34 -0600

    updated api test

--- backend/tests/api.py
+++ backend/tests/api.py
@@ -14,9 +14,42 @@ class TestApi(unittest.TestCase):
 
     def test_get_ski_area(self):
         res = requests.post("http://localhost:7082/get-ski-area",
-                            data={"skiareaname": "Snowbird",
-                                  "api_key": "tmpkey"
-                                  })
+                            data={
+                                "skiareaname": "Snowbird",
+                                "api_key": "tmpkey"
+                            })
+        self.assertTrue(res.ok)
+
+    def test_create_user(self):
+        res = requests.post("http://localhost:7082/create-user",
+                            data={
+                                "username": "andrew",
+                                "email": "andrew@aj-media.net",
+                                "password": "temppassword"
+                            })
+        self.assertTrue(res.ok)
+
+    def test_login(self):
+        res = requests.post("http://localhost:7082/login",
+                            data={
+                                "username": "andrew",
+                                "password": "temppassword"
+                            })
+        self.assertTrue(res.ok)
+
+    def test_logout(self):
+        res = requests.post("http://localhost:7082/logout",
+                            data={
+                                "username": "andrew",
+                            })
+        self.assertTrue(res.ok)
+
+    def test_get_api_key(self):
+        res = requests.post("http://localhost:7082/get-api-key",
+                            data={
+                                "username": "andrew"
+                            })
+        self.assertTrue(res.ok)
 
 
 if __name__ == "__main__":
