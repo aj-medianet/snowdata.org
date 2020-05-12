342c0349db108dc962a78c1c72df824ee0b0e6bb
Rustin Winger <rustinwinger@RustinMBP.local>
2020-05-12 07:23:37 -0700

    updated pricing page

--- frontend/src/components/Pricing.js
+++ frontend/src/components/Pricing.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ReactMarkdown from 'react-markdown';
 import source from '../docs/pricing.md';
+import '../App.css'
 
 // displays website pricing info
 class Pricing extends Component {
@@ -18,15 +19,60 @@ class Pricing extends Component {
     render() {
         const { text } = this.state;
 
-        return (
-            <div className="mt-5">
-                <h1 className="mb-3">Pricing</h1>
-                <div className="m-3">
-                   <h3><ReactMarkdown source={text} /></h3> 
-                </div>   
+        return(
+            <div>
+                <br></br>
+                <h1>Pricing</h1>
+                <br></br>
+                <h3><ReactMarkdown source={text} /></h3>
+                <div class="row">
+                    <div class="column">
+                        <div class="card">
+                            <h1>Tier 1</h1>
+                            <p>This is a description of the specifics of this pricing tier.</p>
+                            <div class="price-bullets">
+                                <li>API Calls per Day</li>
+                                <li>Other Limitation</li>
+                                <li>Price</li>
+                            </div>
+                            <br></br>
+                            <a href="#" class="btn btn-primary">Go</a>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="card">
+                            <h1>Tier 2</h1>
+                            <p>This is a description of the specifics of this pricing tier.</p>
+                            <div class="price-bullets">
+                                <li>API Calls per Day</li>
+                                <li>Other Limitation</li>
+                                <li>Price</li>
+                            </div>
+                            <br></br>
+                            <a href="#" class="btn btn-primary">Go</a>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="card">
+                            <h1>Tier 3</h1>
+                            <p>This is a description of the specifics of this pricing tier.</p>
+                            <div class="price-bullets">
+                                <li>API Calls per Day</li>
+                                <li>Other Limitation</li>
+                                <li>Price</li>
+                            </div>
+                            <br></br>
+                            <a href="#" class="btn btn-primary">Go</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            
+
         )
+
+
+
+
     }
 }
 export default Pricing;
